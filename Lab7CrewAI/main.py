769a0bda42c2d0714a68f4bd488f6c4f2d57ed5d
MoranYo <MoranYosef@gmail.com>
2025-12-28 19:25:21 +0000

    Add all changes

--- Lab7CrewAI/main.py
+++ Lab7CrewAI/main.py
@@ -1,76 +1,64 @@
 from crewai import Agent, Crew, Task, Process
 from crewai.tools import BaseTool, tool
 
+from tools import repl, file_read_tool
+
 from dotenv import load_dotenv
 load_dotenv()
 
-############### Tools
-
-class MyCalcTool(BaseTool):
-    name : str = "My Calculator Tool"
-    description : str = "Use this tool to perform math operations"
-    
-    def _run(self, equation : str) -> str:
-        return eval(equation)
-        
-
-@tool("My Calculator Tool",)
-def my_calc_tool(equation : str) -> str:
-    """ Use this tool to perform math operations """
-    return eval(equation)
-
-################### Agent 1
-
-
-math_agent = Agent(
-    role="Math Master",
-    goal="You are able to evaluate math expressions",
-    backstory="You are a math genius with a sense of humor",
-    tools=[MyCalcTool()],
+coding_agent = Agent(
+    role="Coding Expert",
+    goal="You are able to get a coding task and generate python code to handle the task",
+    backstory="You are a python coding expert with experience in data analysis and scientific computing.",
+    tools=[file_read_tool],
     verbose=True
     
 )
 
-task1 = Task(
-    description="{equation}",
-    expected_output="Give full details in bullet points",
-    agent=math_agent
+coding_task = Task(
+    description="""
+    write python code for this problem: {problem}. return only the code with no comment and remarks and make sure
+    it is syntactically correct python code that can be executed directly. Use the file reading tool to read any files if needed.
+    make sure to return answer from the code.
+    """,
+    expected_output="complete python code that solves the problem. make sure to return the result in the code",
+    agent=coding_agent
 )
 
 ##################################### Agent 2
 
 
-writer_agent = Agent(
-    role='Writer',
-    goal="Craft compelling explanations based on the result of the math equations",
-    backstory="""You are renowed content strategist, known for insightful and engaging
-                articles. You transform complex concepts into compelling narratives.
+Code_runner_agent = Agent(
+    role='Code Runner',
+    goal="Execute the python code provided by the coding agent and return the result",
+    backstory="""You are a python code execution expert with experience in data analysis and scientific computing.
+                You are able to run python code and return the result.
                 """,
+    tools=[repl],
     verbose=True,
 )
 
-task2 = Task(
-    description="Using the insights provided, explain in great detail how the equation and result formed",
-    expected_output="Explain in great detail, save the result in markdown. Do not add the triple tick marks at the begining or the end of the file.",
-    output_file="math.md",
-    agent=writer_agent,
-    
+execution_task = Task(
+    description="execute the python code",
+    expected_output="the actual execution result",
+    agent=Code_runner_agent,
+    context=[coding_task]
 )
 
 
 # ################# Crew
 
 crew = Crew(
-    agents=[math_agent, writer_agent],
-    tasks=[task1, task2],
+    agents=[coding_agent, Code_runner_agent],
+    tasks=[coding_task, execution_task],
     verbose=True,
     process=Process.sequential
 )
 
 
 
-math_input = input("Enter math expression: ")
+inputs = {'problem': "Analyze the data in the file people.csv and provide a summary of the findings."}
 
-result = crew.kickoff(inputs={'equation': math_input })
+result = crew.kickoff(inputs=inputs)
 print('##############################################################')
 print(result)
